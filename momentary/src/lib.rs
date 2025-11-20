fd834ae44f543cc40492f10954391b7c97c4e658
Steve Work <steve@work.renlabs.com>
2025-11-19 17:34:20 -0800

    Use web-time, it implements Instant and Duration in wasm32-unknown-unknown

--- momentary/src/lib.rs
+++ momentary/src/lib.rs
@@ -1,7 +1,10 @@
 use core::panic;
-use std::time::{Duration, Instant};
-
 use log::warn;
+
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::{Duration, Instant};
+#[cfg(target_arch = "wasm32")]
+use web_time::{Duration, Instant};
 
 const SWITCHES: usize = 16;
 const OUTPUTS: usize = 16;
