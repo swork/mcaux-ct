7fae6c460e1b4bf1eff7ae5ea2b0aab7ee5c1fc0
Steve Work <steve@work.renlabs.com>
2026-02-27 10:41:52 -0800

    Unify mcaux-boot. Memory maps and objdumps look right but not yet working.

--- mcaux-app/memory-rp2040.x
+++ mcaux-app/memory-rp2040.x
@@ -1,54 +1,51 @@
-/* Settings here must coordinate with ../mcaux-rp2040-boot/memory.x:
-  - LOADER here is FLASH there
-  - FLASH here is ACTIVE there
- */
-
 MEMORY
 {
-BOOT2            : ORIGIN = 0x10000000, LENGTH = 0x100
-LOADER           : ORIGIN = 0x10000100, LENGTH = 64K - 0x100
-BOOTLOADER_STATE : ORIGIN = 0x10010000, LENGTH = 4K
-FLASH            : ORIGIN = 0x10011000, LENGTH = 512K
-DFU              : ORIGIN = 0x10091000, LENGTH = 516K
-UTILITY          : ORIGIN = 0x10112000, LENGTH = 256K
-RAM              : ORIGIN = 0x20000000, LENGTH = 264K
+  ZERO             : ORIGIN = 0x10000000, LENGTH = 0
+  BOOT2            : ORIGIN = 0x10000000, LENGTH = 0x100
+  LOADER           : ORIGIN = 0x10000100, LENGTH = 64K - 0x100
+  BOOTLOADER_STATE : ORIGIN = 0x10010000, LENGTH = 4K
+  FLASH            : ORIGIN = 0x10011000, LENGTH = 512K
+  DFU              : ORIGIN = 0x10091000, LENGTH = 516K
+  UTILITY          : ORIGIN = 0x10112000, LENGTH = 256K
+  RAM      : ORIGIN = 0x20000000, LENGTH = 264K
 }
 
 SECTIONS {
-.start_block : ALIGN(4)
-{
-__start_block_addr = .;
-KEEP(*(.start_block));
-KEEP(*(.boot_info));
-} > FLASH
+  .start_block : ALIGN(4)
+  {
+    __start_block_addr = .;
+    KEEP(*(.start_block));
+  } > FLASH
 } INSERT AFTER .vector_table;
 
-/* move .text to start /after/ the boot info */
 _stext = ADDR(.start_block) + SIZEOF(.start_block);
 
 SECTIONS {
-.end_block : ALIGN(4)
-{
-__end_block_addr = .;
-KEEP(*(.end_block));
-} > FLASH
+  .end_block : ALIGN(4)
+  {
+    __end_block_addr = .;
+    KEEP(*(.end_block));
+  } > FLASH
 } INSERT AFTER .uninit;
 
 SECTIONS {
-.utility_block : ALIGN(4)
-{
-__utility_block = .;
-KEEP(*(.utility_block));
-} > UTILITY
+  .utility_block : ALIGN(4)
+  {
+    __utility_block = .;
+    KEEP(*(.utility_block));
+  } > UTILITY
 }
-
-__bootloader_state_start = ORIGIN(BOOTLOADER_STATE) - ORIGIN(BOOT2);
-__bootloader_state_end = ORIGIN(BOOTLOADER_STATE) + LENGTH(BOOTLOADER_STATE) - ORIGIN(BOOT2);
-
-__bootloader_dfu_start = ORIGIN(DFU) - ORIGIN(BOOT2);
-__bootloader_dfu_end = ORIGIN(DFU) + LENGTH(DFU) - ORIGIN(BOOT2);
 
 PROVIDE(start_to_end = __end_block_addr - __start_block_addr);
 PROVIDE(end_to_start = __start_block_addr - __end_block_addr);
+
+__bootloader_state_start = ORIGIN(BOOTLOADER_STATE) - ORIGIN(ZERO);
+__bootloader_state_end = ORIGIN(BOOTLOADER_STATE) + LENGTH(BOOTLOADER_STATE) - ORIGIN(ZERO);
+
+__bootloader_active_start = ORIGIN(FLASH) - ORIGIN(ZERO);
+__bootloader_active_end = ORIGIN(FLASH) + LENGTH(FLASH) - ORIGIN(ZERO);
+
+__bootloader_dfu_start = ORIGIN(DFU) - ORIGIN(ZERO);
+__bootloader_dfu_end = ORIGIN(DFU) + LENGTH(DFU) - ORIGIN(ZERO);
 
 __utility_start = ORIGIN(UTILITY);
--- mcaux-boot/build.rs
+++ mcaux-boot/build.rs
@@ -1,28 +1,54 @@
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::path::PathBuf;
 
 fn main() {
-    // Put `memory.x` in our output directory and ensure it's
-    // on the linker search path.
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
+    println!("cargo:warning=CARGO_CFG_FEATURE={}", env::var("CARGO_CFG_FEATURE").unwrap());
+    println!("cargo:warning=CARGO_FEATURE_RP235XA={}", env::var("CARGO_FEATURE_RP235XA").unwrap());
+
+    #[cfg(all(feature = "rp2040", feature = "rp235xa"))]
+    compile_error!("feature \"rp2040\" and feature \"rp235xa\" must not both be specified");
+    #[cfg(not(any(feature = "rp2040", feature = "rp235xa")))]
+    compile_error!("one or the other of feature \"rp2040\" and \"rp235xa\" must be specified");
+
+
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out.display());
 
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
+    let memory_x = if env::var("CARGO_FEATURE_RP235XA").is_ok() {
+        include_str!("../mcaux-app/memory-rp235xa.x")
+    } else {
+        include_str!("../mcaux-app/memory-rp2040.x")
+    };
+
+    // Adjust section names for bootloader
+    let memory_x = memory_x.replace("LENGTH(FLASH)", "LENGTH(ACTIVE)");
+    let memory_x = memory_x.replace("ORIGIN(FLASH)", "ORIGIN(ACTIVE)");
+    let memory_x = memory_x.replacen("FLASH", "ACTIVE", 1);
+    let memory_x = memory_x.replace(" LOADER ", " FLASH ");
+
+    let fname = out.join("memory.x");
+    println!("cargo:warning=filename: {:?}", fname);
+    let mut f = File::create(fname).unwrap();
+    f.write_all(memory_x.as_bytes()).unwrap();
+
+    if env::var("CARGO_FEATURE_RP235XA").is_ok() {
+        println!("cargo:rerun-if-changed=../mcaux-app/memory-rp235xa.x");
+    } else {
+        println!("cargo:rerun-if-changed=../mcaux-app/memory-rp2040.x");
+    }
+
+    println!("cargo:rerun-if-changed=./build.rs");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
     if env::var("CARGO_FEATURE_DEFMT").is_ok() {
+        println!("cargo:warning=Linking with defmt");
         println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    }
+    if env::var("CARGO_FEATURE_RP2040").is_ok() {
+        println!("cargo:warning=Linking with link-rp.x");
+        println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
     }
 }
